1e95d4ebe1be4fa259e52ef49910e12bf2f735a2
nwoolsey <47374704+nwoolsey@users.noreply.github.com>
2019-02-09 22:19:25 -0700

    Add files via upload

---
+++ DecisionTree/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Homework 1 Part 2 Num 2         
+python3.6 DecisionTree_HW01_2_2.py
+echo Homework 1 Part 2 Num 3a         
+python3.6 DecisionTree_HW01_2_3a.py
+echo Homework 1 Part 2 Num 3b         
+python3.6 DecisionTree_HW01_2_3b.py
