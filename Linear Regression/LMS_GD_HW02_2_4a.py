1a527fd980a44678659c5da40b2cdad81f9a9f8c
nwoolsey <47374704+nwoolsey@users.noreply.github.com>
2019-03-05 11:15:21 -0700

    Add files via upload

---
+++ Linear Regression/LMS_GD_HW02_2_4a.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 19 21:58:46 2019
+
+@author: nicho
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Learning rate
+r=float(1/(2**7))
+print('Learning rate:', r)
+
+#Tolerance threshold
+tol = 1e-6
+
+# Read training data
+CSVfile = 'train_concrete.csv'
+
+c=0
+with open (CSVfile , 'r') as f :
+    for line in f :
+        
+        terms = line.strip().split(',')
+        data_row = np.zeros(  (  1,np.size(terms)  )  )
+        for i in range(np.size(terms)):
+            data_row[0,i] = float(terms[i])
+        if c == 0:
+            train_data = data_row
+            c=c+1
+            num_attr = np.size(data_row)-1
+        else :
+            train_data = np.vstack((train_data,data_row))
+            
+# Read test data
+CSVfile = 'test_concrete.csv'
+
+c=0
+with open (CSVfile , 'r') as f :
+    for line in f :
+        
+        terms = line.strip().split(',')
+        data_row = np.zeros(  (  1,np.size(terms)  )  )
+        for i in range(np.size(terms)):
+            data_row[0,i] = float(terms[i])
+        if c == 0:
+            test_data = data_row
+            c=c+1
+        else :
+            test_data = np.vstack((test_data,data_row))
+            
+t = 0
+
+w = np.zeros((num_attr+1,))
+
+J = np.zeros((1,))
+
+tol_acc = 0
+#SGD
+while t < 1e4 and tol_acc == 0 :
+        
+    grad = np.zeros((num_attr+1,))
+    
+    for j in range(num_attr+1) :
+        for i in range(np.shape(train_data)[0]) :
+            if j == num_attr :
+                grad[j] = grad[j] - (train_data[i,num_attr] - np.dot(w,np.hstack((train_data[i,0:num_attr], 1))))
+            else :
+                grad[j] = grad[j] - (train_data[i,num_attr] - np.dot(w,np.hstack((train_data[i,0:num_attr], 1))) )*train_data[i,j]
+    
+    w_new = w - r*grad
+    
+    if np.linalg.norm(w_new-w) < tol:
+        tol_acc = 1
+    w = w_new    
+    if t > 0:
+        J = np.vstack((J,0))
+    for i in range(np.shape(train_data)[0]) :
+        J[t] = J[t] + 0.5*(train_data[i,num_attr] - np.dot(w,np.hstack((train_data[i,0:num_attr], 1))) )**2
+    t = t + 1
+    
+J_test = float(0)
+for i in range(np.shape(test_data)[0]) :
+    J_test = J_test + 0.5*(test_data[i,num_attr] - np.dot(w,np.hstack((test_data[i,0:num_attr], 1))) )**2
+
+print('Learning rate: ', r)
+print('weights: ', w)
+print('Test Cost Function: ', J_test)
+print('\n******************Please close the figure for the code to proceed******************\n') 
+fig = plt.figure()
+plt.plot(J)
+plt.xlabel('Num. Iterations')
+plt.ylabel('Cost Function (training), J')
+plt.title('Gradient Descent')
+plt.show()
---
+++ Linear Regression/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo Homework 2 Part 2 Num 2a         
+python3.6 DecisionTree_HW02_2_2a.py
+echo Homework 2 Part 2 Num 2b         
+python3.6 DecisionTree_HW02_2_2b.py
+echo Homework 2 Part 2 Num 2c         
+python3.6 DecisionTree_HW02_2_2c.py
+echo Homework 2 Part 2 Num 2d         
+python3.6 DecisionTree_HW02_2_2d.py
+echo Homework 2 Part 2 Num 2e         
+python3.6 DecisionTree_HW02_2_2e.py
+echo Homework 2 Part 2 Num 4a         
+python3.6 LMS_GD_HW02_2_4a.py
+echo Homework 2 Part 2 Num 4b         
+python3.6 LMS_SGD_HW02_2_4b.py
+echo Homework 2 Part 2 Num 4c         
+python3.6 ExSol_HW02_2_4c.py
